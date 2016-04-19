da1d164c62a7fd971c65ac72e04805fadc5f1355
666dsa666 <findelafantasy@hotmail.fr>
2016-04-19 22:01:33 +0200

    Update CheeseMaking.ino

--- CheeseMaking/CheeseMaking.ino
+++ CheeseMaking/CheeseMaking.ino
@@ -1,10 +1,9 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
-//test
 #define DHTPIN 2
 #define DHTTYPE DHT11
-#define RELAY1 10
-#define RELAY2 11
+#define RELAYTEMP 10
+#define RELAYHUMI 11
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -12,148 +11,183 @@
 #define btnSELECT 4
 #define btnNONE   5
 
-int lcd_key = 0;
-int adc_key_in = 0;
 int selected = 1;
-int tset = constrain(40, 10, 60);
-int hset = constrain(20, 0, 100);
-int varsetTempHumi = 0;
-int HeaterON = 0;
-int HumidifierON = 0;
+int x;
+unsigned long counter = 5000;
+int tset = 40;
+int hset = 50;
+int lastt = 0;
+int lasth = 0;
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
 
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);        // select the pins used on the LCD panel
-DHT dht(DHTPIN, DHTTYPE);
+// read the buttons
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0);      // read the value from the sensor
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.1 us this threshold
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 250)  return btnUP;
+  if (adc_key_in < 450)  return btnDOWN;
+  if (adc_key_in < 650)  return btnLEFT;
+  if (adc_key_in < 850)  return btnSELECT;
 
-int read_LCD_buttons(int msTimeOut) {
-  unsigned long time = millis();
-  while (time - millis() < msTimeOut)
-  {
-    adc_key_in = analogRead(0);
-    if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.0 comment the other threshold and use the one below:
+  /*
     if (adc_key_in < 50)   return btnRIGHT;
-    if (adc_key_in < 250)  return btnUP;
-    if (adc_key_in < 450)  return btnDOWN;
-    if (adc_key_in < 650)  return btnLEFT;
-    if ((adc_key_in < 850)  && (varsetTempHumi == 0)) setTempHumi();
-    if ((adc_key_in < 850)  && (varsetTempHumi == 1)) return btnSELECT;
-  }
+    if (adc_key_in < 195)  return btnUP;
+    if (adc_key_in < 380)  return btnDOWN;
+    if (adc_key_in < 555)  return btnLEFT;
+    if (adc_key_in < 790)  return btnSELECT;
+  */
   return btnNONE;  // when all others fail, return this...
 }
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);        // select the pins used on the LCD panel
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   lcd.begin(16, 2);                       // start the library
   dht.begin();
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAYTEMP, OUTPUT);
+  pinMode(RELAYHUMI, OUTPUT);
 }
 
 void loop() {
+  lcd_key = read_LCD_buttons();  // read the buttons
+  if (lcd_key != btnNONE) {
+    varsetTempHumi();
+  }
+  if (millis() >= counter) {
+    counter = millis() + 5000;
+    int h = int(dht.readHumidity());
+    int t = int(dht.readTemperature());
+    if (( t != lastt ) or (h != lasth)) {
+      lastt = t;
+      lasth = h;
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp: ");
+      lcd.setCursor(8, 0);
+      lcd.print("C");
+      lcd.setCursor(0, 1);
+      lcd.print("Humi: ");
+      lcd.setCursor(7, 1);
+      lcd.print(" %");
+      lcd.setCursor(6, 0);
+      lcd.print(t);
+      lcd.setCursor(6, 1);
+      lcd.print(h);
+      if ((t < tset) and (h < hset)) {
+        digitalWrite(RELAYTEMP, HIGH);
+        digitalWrite(RELAYHUMI, HIGH);
+        lcd.setCursor(14, 0);
+        lcd.print("ON");
+        lcd.setCursor(14, 1);
+        lcd.print("ON");
+      }
+      if ((t >= tset) and (h >= hset)) {
+        digitalWrite(RELAYTEMP, LOW);
+        digitalWrite(RELAYHUMI, LOW);
+      }
+      if ((t >= tset) and (h < hset)) {
+        digitalWrite(RELAYTEMP, HIGH);
+        digitalWrite(RELAYHUMI, LOW);
+        lcd.setCursor(14, 1);
+        lcd.print("ON");
+      }
+      if ((t < tset) and (h >= hset)) {
+        digitalWrite(RELAYTEMP, LOW);
+        digitalWrite(RELAYHUMI, HIGH);
+        lcd.setCursor(14, 0);
+        lcd.print("ON");
+      }
+    }
+  }
+}
+
+
+void varsetTempHumi() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.setCursor(8, 0);
+  lcd.print("SetTemp: ");
+  lcd.setCursor(11, 0);
   lcd.print("C");
   lcd.setCursor(0, 1);
-  lcd.print("Humi: ");
-  lcd.setCursor(8, 1);
-  lcd.print(" %");
-  while (HeaterON = 1) {
+  lcd.print("SetHumi: ");
+  lcd.setCursor(11, 1);
+  lcd.print("%");
+  tset = constrain(tset, 10, 60);
+  hset = constrain(hset, 10, 99);
+  lcd.setCursor(9, 0);
+  lcd.print(tset);
+  lcd.setCursor(9, 1);
+  lcd.print(hset);
+  if (selected == 1) {
     lcd.setCursor(15, 0);
     lcd.cursor();
     lcd.blink();
   }
-  while (HumidifierON = 1) {
+  if (selected == 2) {
     lcd.setCursor(15, 1);
     lcd.cursor();
     lcd.blink();
   }
-  delay(3000);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    lcd.print("Failed!");
-    return;
-  }
-  lcd.setCursor(6, 0);
-  lcd.print(t);
-  lcd.setCursor(6, 1);
-  lcd.print(h);
-  if (t < tset) {
-    digitalWrite(RELAY1, LOW);
-    HeaterON = 1;
-  }
-  else {
-    digitalWrite(RELAY1, HIGH);
-  }
-  if (h < hset) {
-    digitalWrite(RELAY2, LOW);
-    HumidifierON = 1;
-  }
-  else {
-    digitalWrite(RELAY2, HIGH);
-  }
-  return;
-}
-
-void setTempHumi() {
-  while (1) {
-    varsetTempHumi = 1;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("SetTemp: ");
-    lcd.setCursor(11, 0);
-    lcd.print("C");
-    lcd.setCursor(0, 1);
-    lcd.print("SetHumi: ");
-    lcd.setCursor(11, 1);
-    lcd.print("%");
-    while (varsetTempHumi == 1) {
-      if (selected == 1) {
-        lcd.setCursor(9, 0);
-        lcd.print(tset);
-        lcd.setCursor(15, 0);
+  lcd_key = read_LCD_buttons();  // read the buttons
+  switch (lcd_key)               // depending on which button was pushed, we perform an action
+  {
+    case btnRIGHT:
+      {
+        if (selected == 1) {
+          tset++;
+        }
+        if (selected == 2) {
+          hset++;
+        }
+        delay(200);
+        break;
       }
-      if (selected == 2) {
-        lcd.setCursor(9, 1);
-        lcd.print(hset);
-        lcd.setCursor(15, 1);
+    case btnLEFT:
+      {
+        if (selected == 1) {
+          tset--;
+        }
+        if (selected == 2) {
+          hset--;
+        }
+        delay(200);
+        break;
       }
-      lcd.cursor();
-      lcd.blink();
-      lcd_key = read_LCD_buttons(5000);  // read the buttons; timeout = 5sec
-      switch (lcd_key) {
-        case btnUP: {
-            selected = 1;
-            break;
-          }
-        case btnDOWN: {
-            selected = 2;
-            break;
-          }
-        case btnRIGHT: {
-            if (selected == 1) {
-              tset++;
-            }
-            if (selected == 2) {
-              hset++;
-            }
-            break;
-          }
-        case btnLEFT: {
-            if (selected == 1) {
-              tset--;
-            }
-            if (selected == 2) {
-              hset--;
-            }
-            break;
-          }
-        case btnSELECT: {
-            varsetTempHumi = 0;
-            break;
-          }
+    case btnUP:
+      {
+        selected = 1;
+        delay(200);
+        break;
       }
-    }
+    case btnDOWN:
+      {
+        selected = 2;
+        delay(200);
+        break;
+      }
+    case btnSELECT:
+      {
+        delay(200);
+        break;
+      }
+    case btnNONE:
+      {
+        break;
+      }
   }
   return;
 }
